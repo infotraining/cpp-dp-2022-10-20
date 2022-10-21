abb7f3557e88500ef4d975c7f492e19ada690b03
Krystian Piękoś <krystian.piekos@infotraining.pl>
2022-10-21 14:44:29 +0200

    Decorator + Builder

--- Structural/Decorator.Exercise/main.cpp
+++ Structural/Decorator.Exercise/main.cpp
@@ -7,11 +7,75 @@ void client(std::unique_ptr<Coffee> coffee)
     coffee->prepare();
 }
 
+namespace Starbugs
+{
+    inline namespace VariadicTemplates
+    {
+        template <typename HeadDecorator, typename... TailDecorators>
+        struct DecorateWith
+        {
+            static std::unique_ptr<Coffee> make_decorator(std::unique_ptr<Coffee> component)
+            {
+                auto decorator_component = std::make_unique<HeadDecorator>(move(component));
+
+                return DecorateWith<TailDecorators...>::make_decorator(move(decorator_component));
+            }
+        };
+
+        template <typename HeadDecorator>
+        struct DecorateWith<HeadDecorator>
+        {
+            static std::unique_ptr<Coffee> make_decorator(std::unique_ptr<Coffee> component)
+            {
+                return std::make_unique<HeadDecorator>(move(component));
+            }
+        };
+
+        class CoffeeBuilder
+        {
+            std::unique_ptr<Coffee> coffee_;
+
+        public:
+            template <typename BaseCoffee>
+            CoffeeBuilder& create_base()
+            {
+                coffee_.reset(new BaseCoffee);
+
+                return *this;
+            }
+
+            template <typename... Condiments>
+            CoffeeBuilder& add()
+            {
+                assert(coffee_);
+
+                coffee_ = DecorateWith<Condiments...>::make_decorator(move(coffee_));
+
+                return *this;
+            }
+
+            std::unique_ptr<Coffee> get_coffee()
+            {
+                return std::move(coffee_);
+            }
+        };
+    }
+}
+
+
 int main()
 {
     std::unique_ptr<Coffee> cf = 
         std::make_unique<Whipped>(
             std::make_unique<Whisky>(
-                std::make_unique<Espresso>()));
+                std::make_unique<ExtraEspresso>(
+                    std::make_unique<Espresso>())));
     client(std::move(cf));
+
+    ///////////////////////////////////////////////////////
+    Starbugs::CoffeeBuilder cb;
+    cb.create_base<Espresso>()
+        .add<Whisky, Whisky, ExtraEspresso>()
+        .add<Whipped>();
+    client(cb.get_coffee());
 }
