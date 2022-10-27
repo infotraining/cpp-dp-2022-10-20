1a8b128ec44013eacc631868b940e513bfa7803f
Krystian Piękoś <krystian.piekos@infotraining.pl>
2022-10-27 16:07:54 +0200

    Command

--- Behavioral/Command.Exercise/main.cpp
+++ Behavioral/Command.Exercise/main.cpp
@@ -2,18 +2,29 @@
 
 #include "application.hpp"
 #include "command.hpp"
-#include <boost/di.hpp>
+#include <boost/di.hpp>  // https://github.com/boost-ext/di
 
 using namespace std;
 namespace di = boost::di;
 
 int main()
 {
+    Document doc;
+
     const auto injector = di::make_injector(
         di::bind<Console>().to<Terminal>(),
-        di::bind<Clipboard>().to<SharedClipboard>());
+        di::bind<Clipboard>().to<SharedClipboard>(),
+        di::bind<Document>().to(doc));
+
 
     auto app = injector.create<Application>();
+
+    // Terminal term;
+    // Document doc;
+    // Application app(term);
+
+    // auto print_cmd = std::make_shared<PrintCmd>(doc, term);
+    // app.add_command("Print", print_cmd);
 
     app.add_command("Print"s, injector.create<std::shared_ptr<PrintCmd>>());
     app.add_command("ToUpper"s, injector.create<std::shared_ptr<ToUpperCmd>>());
